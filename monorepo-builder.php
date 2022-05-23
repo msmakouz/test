485f9e5d3cbaf81267bd1ba5813908164063d41c
Maxim Smakouz <m.smakouz@gmail.com>
2022-05-23 20:14:11 +0300

    git init

---
+++ monorepo-builder.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+use MonorepoBuilder\MostRecentTagResolver;
+use MonorepoBuilder\ProcessParser;
+use MonorepoBuilder\TagParserInterface;
+use MonorepoBuilder\ValidateVersionReleaseWorker;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\Contract\Git\TagResolverInterface;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+
+/**
+ * Monorepo Builder additional fields
+ *
+ * @see https://github.com/symplify/symplify/issues/2061
+ */
+\register_shutdown_function(static function () {
+    $dest = \json_decode(\file_get_contents(__DIR__ . '/composer.json'), true);
+
+    $result = [
+        'name'              => 'test/test',
+        'type'              => 'library',
+        'description'       => $dest['description'] ?? '',
+        'require'           => $dest['require'] ?? [],
+        'autoload'          => $dest['autoload'] ?? [],
+        'require-dev'       => $dest['require-dev'] ?? [],
+        'minimum-stability' => $dest['minimum-stability'] ?? 'dev',
+        'prefer-stable'     => $dest['prefer-stable'] ?? true,
+    ];
+
+    $json = \json_encode($result, \JSON_PRETTY_PRINT | \JSON_UNESCAPED_SLASHES);
+
+    \file_put_contents(__DIR__ . '/composer.json', $json . "\n");
+});
+
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::PACKAGE_ALIAS_FORMAT, '<major>.<minor>.x-dev');
+    $parameters->set(Option::PACKAGE_DIRECTORIES, ['src']);
+    $parameters->set(Option::DEFAULT_BRANCH_NAME, '1.6');
+
+    $services = $containerConfigurator->services();
+
+    $services->set(TagResolverInterface::class, MostRecentTagResolver::class);
+    $services->set(TagParserInterface::class, ProcessParser::class)->autowire();
+
+    # release workers - in order to execute
+    $services->set(ValidateVersionReleaseWorker::class);
+    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
+    $services->set(AddTagToChangelogReleaseWorker::class);
+    $services->set(TagVersionReleaseWorker::class);
+    $services->set(PushTagReleaseWorker::class);
+    $services->set(SetNextMutualDependenciesReleaseWorker::class);
+    $services->set(UpdateBranchAliasReleaseWorker::class);
+    $services->set(PushNextDevReleaseWorker::class);
+};
